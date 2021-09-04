8ee3733bdb4c26a06c0e79dfbb87dc513b61c5de
hslim <hslim@elecvery.com>
2021-09-04 16:06:53 +0900

    icon 추가, CountFragment UI 생성

--- app/src/main/java/com/myohoon/hometrainingautocounter/view/fragment/main/GoalsSettingFragment.kt
+++ app/src/main/java/com/myohoon/hometrainingautocounter/view/fragment/main/GoalsSettingFragment.kt
@@ -5,10 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.Observable
 import androidx.fragment.app.activityViewModels
 import com.myohoon.hometrainingautocounter.R
 import com.myohoon.hometrainingautocounter.databinding.FragmentGoalsSettingBinding
 import com.myohoon.hometrainingautocounter.databinding.FragmentMainBinding
+import com.myohoon.hometrainingautocounter.view.MainActivity
 import com.myohoon.hometrainingautocounter.view.adapter.ExerciseAdapter
 import com.myohoon.hometrainingautocounter.viewmodel.ExerciseViewModel
 
@@ -24,17 +26,40 @@ class GoalsSettingFragment : Fragment() {
     //viewModel
     private val exerciseVM by activityViewModels<ExerciseViewModel>()
 
+    //observable callback
+    private lateinit var goCountFragmentCB: Observable.OnPropertyChangedCallback
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         //view
         _binding = FragmentGoalsSettingBinding.inflate(inflater, container, false)
+        binding.exerciseVM = exerciseVM
 
+        initObservableCallback()
         return binding.root
+    }
+
+    private fun initObservableCallback() {
+        goCountFragmentCB = object : Observable.OnPropertyChangedCallback() {
+            override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
+                if (exerciseVM.goCountFragment.get()){
+                    exerciseVM.goCountFragment.set(false)
+                    MainActivity.addFragmentInMain(CountFragment(), CountFragment.TAG)
+                }
+            }
+        }
+
+        exerciseVM.goCountFragment.addOnPropertyChangedCallback(goCountFragmentCB)
+    }
+
+    private fun removeObservableCallback() {
+        exerciseVM.goCountFragment.removeOnPropertyChangedCallback(goCountFragmentCB)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        removeObservableCallback()
         _binding = null
     }
 }
--- app/src/main/java/com/myohoon/hometrainingautocounter/viewmodel/ExerciseViewModel.kt
+++ app/src/main/java/com/myohoon/hometrainingautocounter/viewmodel/ExerciseViewModel.kt
@@ -1,13 +1,22 @@
 package com.myohoon.hometrainingautocounter.viewmodel
 
+import android.view.View
+import androidx.databinding.ObservableBoolean
 import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
 import com.myohoon.hometrainingautocounter.repository.enums.Exercise
 
-class ExerciseViewModel(): ViewModel() {
+class ExerciseViewModel: ViewModel() {
 
     //운동 목록
     val exerciseList = ObservableField(enumValues<Exercise>().toList())
+
+    val goCountFragment = ObservableBoolean(false)
+
+    fun btnStartExerciseClicked(view: View){
+        goCountFragment.set(true)
+    }
+
 
     override fun onCleared() {
         super.onCleared()
